e2b261a0f1b825138f10b181bc2fa32d26de9fb3
Mihai9088 <mihaitza1058@gmail.com>
2025-02-11 16:57:04 +0200

    added roles and permissions

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Enums\RolesEnum;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@example.com',
+        ])->assignRole(RolesEnum::User->value);
+
+        User::factory()->create([
+            'name' => 'vendor',
+            'email' => 'vendor@example.com',
+        ])->assignRole(RolesEnum::Vendor->value);
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+        ])->assignRole(RolesEnum::Admin->value);
+    }
+}
